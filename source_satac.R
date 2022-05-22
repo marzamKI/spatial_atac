540735885cfdda09c79c7396dc7b5007482c2a87
Margherita <margherita.zamboni@ki.se>
2022-05-22 20:59:40 +0200

    create source file

---
+++ source_satac.R
@@ -0,0 +1,27 @@
+# load packages
+# omics
+library(GenomeInfoDb)
+library(EnsDb.Mmusculus.v79)
+library(Signac)
+library(Seurat)
+library(DropletUtils)
+
+#spatial
+library(STutility)
+library(parallel)
+library(ica)
+library(spdep)
+library(jpeg)
+
+#plots
+library(ggplot2)
+library(patchwork)
+library(scales)
+library(RColorBrewer)
+library(viridis)
+library(ggpubr)
+
+#other
+library(dplyr)
+library(purrr)
+library(Matrix)
