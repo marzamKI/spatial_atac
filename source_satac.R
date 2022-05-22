52fee7eb66c5c168bcfb57803a84fdfc0f1d5dbc
Margherita <margherita.zamboni@ki.se>
2022-05-22 21:05:08 +0200

    add color palettes

--- source_satac.R
+++ source_satac.R
@@ -1,4 +1,5 @@
-# load packages
+### load packages
+
 # omics
 library(GenomeInfoDb)
 library(EnsDb.Mmusculus.v79)
@@ -25,3 +26,15 @@ library(ggpubr)
 library(dplyr)
 library(purrr)
 library(Matrix)
+
+###color palettes
+magenta_scale <- c("gray93", "#FFF7F3","#FDE0DD","#FCC5C0","#FA9FB5","#F768A1","#DD3497","#AE017E")
+cyan_scale <- c("gray93","#F7FCF0","#E0F3DB","#CCEBC5","#7BCCC4","#4EB3D3","#2B8CBE","#0868AC","#084081","#084081","#084081")
+
+#embryo
+bright_visium <- c("#4477AA","#58BEC8","#9ECAE1","#669C39","#669C39","#CCBB44","#2F9558", "#B73D77","#E05B77","#B06992","#A8A8A8","#E08762")
+color_sections <- c("#E69F00", "#D55E00", "#aaaa00", "#009E73","#56B4E9", "#0072B2")
+
+#brca
+
+
