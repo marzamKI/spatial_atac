7f0ad1d9142a4325ce045e8bc271841630be87f1
Margherita <margherita.zamboni@ki.se>
2022-05-31 13:07:43 +0200

    add comparison between visium original and dca

--- embryo_visium_integration.R
+++ embryo_visium_integration.R
@@ -1,7 +1,7 @@
 source("source_satac.R")
 # visium integration
 
-#process all visium sections
+#process all visium visiumctions
 infoTable <- data.frame(
   samples = c("V10B01-135_D1_manual_alignment_v100/mouse_filtered_feature_bc_matrix.h5",
               "V10S29-086_D1_spaceranger/outs/mouse_feature_bc_matrix.h5",
@@ -17,39 +17,39 @@ infoTable <- data.frame(
            "220421_Visium_e12/220421_e12_B1/outs/spatial/scalefactors_json.json")
 )
 
-se <- InputFromTable(infotable = infoTable, 
+visium <- InputFromTable(infotable = infoTable, 
                      min.gene.count = 0, 
                      min.gene.spots = 0,
                      min.spot.count = 0,
                      platform =  "Visium")
 
-se <- LoadImages(se, time.resolve = FALSE, verbose = TRUE)
-se$section <- se@tools$Staffli@meta.data$sample
+visium <- LoadImages(visium, time.resolve = FALvisium, verbovisium = TRUE)
+visium$visiumction <- visium@tools$Staffli@meta.data$sample
 
 # Perform standard analysis of each modality independently 
 # RNA analysis
-se <- NormalizeData(se) %>%
+visium <- NormalizeData(visium) %>%
   FindVariableFeatures() %>%
   ScaleData() %>%
   RunPCA()
 
-ElbowPlot(se)
+ElbowPlot(visium)
 
-se <- RunHarmony(se, 
+visium <- RunHarmony(visium, 
                  group.by.vars = "section", 
                  reduction = "pca", dims.use = 1:15, 
                  assay.use = "RNA", 
                  project.dim = F,
-                 verbose = T) %>%
+                 verbovisium = T) %>%
   RunUMAP(reduction = "harmony", dims = 1:15, reduction.name = "umap.harmony") %>%
   FindNeighbors(reduction = "harmony", dims = 1:15) %>%
   FindClusters(resolution = 0.3)
-se$seurat_clusters_harmony <- se$seurat_clusters
+visium$seurat_clusters_harmony <- visium$seurat_clusters
 
-saveRDS(se, "visium_allages.rds")
-write.csv(se@assays$RNA@counts, "visium_counts.csv")
+saveRDS(visium, "visium_allages.rds")
+write.csv(visium@assays$RNA@counts, "visium_counts.csv")
 
-### denoise with DCA and default parameters ###
+### denoivisium with DCA and default parameters ###
 mtx <- read.table("visium_denoised/mean.tsv")
 colnames(mtx) <- gsub("\\.", "-", colnames(mtx))
 visium[["RNA_dca"]] <- CreateAssayObject(counts = as.matrix(mtx))
@@ -136,3 +136,41 @@ DotPlot(combined, features = signatures, cols = c(cyan_scale[1], cyan_scale[8]))
 signatures <- paste0("Cluster",c("6","8","3","1", "11", "2","4","5","9","10","7"))
 DotPlot(visium, features = signatures, cols = c(magenta_scale[1], magenta_scale[8]))
 
+
+# clustering on DCA
+DefaultAssay(visium) <- "RNA_dca"
+visium <- NormalizeData(visium) %>%
+  FindVariableFeatures() %>%
+  ScaleData() %>%
+  RunPCA()
+
+ElbowPlot(visium)
+
+visium <- RunHarmony(visium, 
+                 group.by.vars = "section", 
+                 reduction = "pca", dims.use = 1:15, 
+                 assay.use = "RNA_dca", 
+                 project.dim = F,
+                 verbovisium = T) %>%
+  RunUMAP(reduction = "harmony", dims = 1:15, reduction.name = "umap.harmony") %>%
+  FindNeighbors(reduction = "harmony", dims = 1:15) %>%
+  FindClusters(resolution = 0.3)
+visium$seurat_clusters_harmony_dca <- visium$seurat_clusters
+
+Idents(visium) <- "RNA_snn_res.0.3"
+levels(visium) <- c("3", "2", "1","4","9","11", "6","8","0", "5","10","7")
+
+Idents(visium) <- "seurat_clusters_harmony_dca"
+levels(visium) <- c("4", "2", "3","7","5", "11","1", "6","0","12","8","10","9")
+visium$seurat_clusters_harmony_dca <- Idents(visium)
+
+p1 <- ST.FeaturePlot(visium, "ident", ncol = 3, cols = bright_visium)
+p2 <- ST.FeaturePlot(visium, "seurat_clusters_harmony_dca", ncol = 3, cols = visium_dca)
+p1+p2
+
+Idents(visium) <- "seurat_clusters_harmony_dca"
+prop <- prop.table(table(visium$seurat_clusters_harmony_dca, visium@active.ident), 1)*100
+heatmap.2(prop[order(nrow(prop):1),], 
+          Colv = NA, Rowv = NA, scale="none", 
+          xlab="peaks cluster", ylab="dca clusters", 
+          col = hm_colors, RowSideColors=rev(visium_dca), ColSideColors = bright_visium)
--- source_satac.R
+++ source_satac.R
@@ -34,9 +34,12 @@ library(Matrix)
 ###color palettes
 magenta_scale <- c("gray93", "#FFF7F3","#FDE0DD","#FCC5C0","#FA9FB5","#F768A1","#DD3497","#AE017E")
 cyan_scale <- c("gray93","#F7FCF0","#E0F3DB","#CCEBC5","#7BCCC4","#4EB3D3","#2B8CBE","#0868AC","#084081","#084081","#084081")
+fun_color_range <- colorRampPalette(c("gray93", "#AE017E"))  # Create color generating function
+hm_colors <- fun_color_range(100)                         
 
 #embryo
-bright_visium <- c("#4477AA","#58BEC8","#9ECAE1","#669C39","#669C39","#CCBB44","#2F9558", "#B73D77","#E05B77","#B06992","#A8A8A8","#E08762")
+bright_visium <- c("#4477AA",  "#9ECAE1","#669C39", "#58BEC8", "#ccddaa", "#CCBB44", "#2F9558", "#B73D77","#E05B77", "#B06992", "#A8A8A8", "#E08762")
+visium_dca <- c("#4477AA","#58BEC8","#669C39","#9ECAE1","#ccddaa","#CCBB44","#2F9558","#cc6677","#E05B77","#B73D77","#B06992","#A8A8A8","#E08762")
 color_sections <- c("#E69F00", "#D55E00", "#aaaa00", "#009E73","#56B4E9", "#0072B2")
 cols <-  c("#4477AA","#58AAD2","#58BEC8", "#2F9558","#669C39","#CCBB44", "#A8A8A8","#E05B77","#B73D77", "#B06992","#E08762") #embryo clusters res 0.7
 cols_dca <-  c("#4477AA","#58AAD2","#58BEC8", "#2F9558","#669C39","#CCBB44", "#E05B77","#B73D77", "#B06992","#E08762")
